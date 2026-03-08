23f6d668453545a02947ccb2c7b1f30220d9c832
rachid1082 <boudaourachid1@gmail.com>
2026-03-08 11:47:37 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
