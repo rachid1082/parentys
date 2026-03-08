ddab01967f1728d88b1039b0929bbd8e59e37a63
rachid1082 <boudaourachid1@gmail.com>
2026-03-08 14:48:25 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
 
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
-};
+export default config
